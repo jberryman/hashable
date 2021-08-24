dcc38c70ca7f2d61cf58a4ce272b639c2b769c01
Brandon Simmons <brandon.m.simmons@gmail.com>
2021-08-24 11:25:25 -0400

    Compatibility with https://github.com/haskell/text/pull/365

--- src/Data/Hashable/Class.hs
+++ src/Data/Hashable/Class.hs
@@ -711,7 +711,7 @@ instance Hashable BSI.ShortByteString where
 
 instance Hashable T.Text where
     hashWithSalt salt (T.Text arr off len) =
-        hashByteArrayWithSalt (TA.aBA arr) (off `shiftL` 1) (len `shiftL` 1)
+        hashByteArrayWithSalt (TA.aBA arr) off len
         salt
 
 instance Hashable TL.Text where
